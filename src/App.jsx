81ca2f77d6c965c133514decadc7205a9d7e7259
ad.hanif <ad.hanif@outlook.com>
2023-05-28 17:30:30 +0200

    restore tried

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { useEffect, useState } from "react";
+
 import "./App.css";
 import axios from "axios";
 import PostList from "./assets/components/PostList";
 import Form1 from "./assets/components/Form";
-import Navbar from "./assets/components/Navbar";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 function App() {
   const url = "https://jsonplaceholder.typicode.com/";
   const [postLists, setPostLists] = useState([]);
- 
 
   const getData = () => {
     axios
@@ -32,21 +28,10 @@ function App() {
 
   return (
     <>
-      <Navbar />
-      <Container>
-        <Row className="justify-content-center">
-          <Form1 setPostLists={setPostLists} postLists={postLists} />
-        </Row>
-        <Row className="justify-content-center">
-          <PostList
-            postLists={postLists}
-            setPostLists={setPostLists}
-            
-          />
-        </Row>
-      </Container>
+      <Form1 setPostLists={setPostLists} postLists={postLists} />
+      <PostList postLists={postLists} setPostLists={setPostLists} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
